fdd1cdc94132855101d7623c2d5a70832cafbeba
Deltx <larroyobjol@gmail.com>
2022-07-31 12:51:37 -0500

    objeto triangulo creado

---
+++ src/misClases/Triangulo.java
@@ -0,0 +1,18 @@
+package misClases;
+
+public class Triangulo {
+
+    private float base;
+    private float altura;
+
+    // constructor vacío
+    public Triangulo() {
+
+    }
+
+    public Triangulo(float base, float altura) {
+        this.altura = altura;
+        this.base = base;
+    }
+
+}
