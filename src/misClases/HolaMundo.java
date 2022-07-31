88b28709b4ef199a88eace9e15d09a2f93e0d405
Deltx <larroyobjol@gmail.com>
2022-07-31 00:26:36 -0500

    agregados comentarios

--- src/misClases/HolaMundo.java
+++ src/misClases/HolaMundo.java
@@ -1,6 +1,22 @@
 package misClases;
 
 public class HolaMundo {
-    
-    
+
+    private String txt = "Hello World";
+
+    //Imprime en pantalla el mensaje
+    public void Escribir() {
+
+        System.out.println(txt + ":)");
+
+    }
+
+    public String getTxt() {
+        return txt;
+    }
+
+    public void setTxt(String txt) {
+        this.txt = txt;
+    }
+
 }
