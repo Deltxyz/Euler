5cb27cbc793683b7152eb123c8d86fbd7a796405
Deltx <larroyobjol@gmail.com>
2022-07-31 11:46:17 -0500

    Este commit contiene git ignore

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,12 +1,14 @@
 package test;
 
 import misClases.EvenFibonacciNumbers;
+import misClases.HolaMundo;
 import misClases.LargestPrimeFactor;
 import misClases.MultiplesOf3Or5;
 
 public class Test {
 
     public static void main(String[] args) {
+        
 
         MultiplesOf3Or5 mult = new MultiplesOf3Or5();
         //System.out.println("La suma de multiplos es: " + mult.SumarMultiplos(1000));
@@ -17,6 +19,11 @@ public class Test {
         LargestPrimeFactor large = new LargestPrimeFactor();
         System.out.println("El mayor primo de 600851475143 es: " + large.MayorPrimo(1));
 
+        HolaMundo holaMundo = new HolaMundo();
+        holaMundo.Escribir();
+
+
+        //Holas
     }
 
 }
