732e8eaaa76f62d9551a2947cee101d68fdc3c2f
Deltx <larroyobjol@gmail.com>
2022-07-31 06:36:19 +0100

    Hola Mundo con constructores

--- src/misClases/HolaMundo.java
+++ src/misClases/HolaMundo.java
@@ -3,6 +3,18 @@ package misClases;
 public class HolaMundo {
 
     private String txt = "Hello World";
+    
+    //Constructor Vacío
+    public HolaMundo() {
+
+    }
+
+    //Constructor con argumentos
+    public HolaMundo(String txt) {
+
+        this.txt = txt;
+
+    }
 
     //Imprime en pantalla el mensaje
     public void Escribir() {
